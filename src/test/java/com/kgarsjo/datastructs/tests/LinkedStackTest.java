5266c1c25fb84b44fc6498110bc4988fc2424b57
Kevin Garsjo <kgarsjo@gmail.com>
2016-06-28 21:41:33 -0700

    Add a linked-list backe implementation of Stack
    The Stack wraps a head Node, where Node wraps a payload and a next-node pointer. Operations append a new node as head or push head to the next node

---
+++ src/test/java/com/kgarsjo/datastructs/tests/LinkedStackTest.java
@@ -0,0 +1,13 @@
+package com.kgarsjo.datastructs.tests;
+
+import com.kgarsjo.datastructs.LinkedStack;
+import org.junit.Before;
+
+public class LinkedStackTest extends AbstractStackTest {
+
+    @Before
+    public void setUp() {
+        testObject = new LinkedStack<>();
+    }
+
+}
