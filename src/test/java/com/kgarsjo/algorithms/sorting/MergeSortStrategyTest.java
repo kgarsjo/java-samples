4f6e3325f5d4ed76fab841ea32f16bc705c9110d
Kevin Garsjo <kgarsjo@gmail.com>
2016-06-29 21:54:58 -0700

    Implement mergesort

---
+++ src/test/java/com/kgarsjo/algorithms/sorting/MergeSortStrategyTest.java
@@ -0,0 +1,11 @@
+package com.kgarsjo.algorithms.sorting;
+
+import org.junit.Before;
+
+public class MergeSortStrategyTest extends AbstractSortStrategyTest {
+
+    @Before
+    public void setUp() {
+        testObject = new MergeSortStrategy<>();
+    }
+}
