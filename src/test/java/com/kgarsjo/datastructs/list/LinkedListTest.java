abecb53eee4722fe7da7787ac6e868e6cdb18ecf
kgarsjo <krgarsjo@gmail.com>
2016-06-30 22:49:43 -0700

    Add a linked list implementation

---
+++ src/test/java/com/kgarsjo/datastructs/list/LinkedListTest.java
@@ -0,0 +1,9 @@
+package com.kgarsjo.datastructs.list;
+
+public class LinkedListTest extends AbstractListTest {
+
+    protected IList<String> createList() {
+        return new LinkedList<>();
+    }
+
+}
